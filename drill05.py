4c8cfc46825f00817b6d2700993d01e3ddd85a4c
jysabina03 <jysabina03@tukorea.ac.kr>
2023-10-03 20:55:49 +0900

    랜덤 위치에 손 화살표 출력

--- drill05.py
+++ drill05.py
@@ -1,13 +1,15 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
 
 def load_resources():
-    global TUK_ground, character
+    global TUK_ground, character, arrow
+
     TUK_ground = load_image('TUK_GROUND.png')
     character = load_image('animation_sheet.png')
-
+    arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
@@ -21,15 +23,18 @@ def handle_events():
 
 
 def reset_world():
-    global running, x, y, frame
+    global running, x, y, frame,hx,hy
     running = True
     x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
 
+    # hx, hy = TUK_WIDTH-50,TUK_HEIGHT-50
+    hx, hy = random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT)
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    arrow.draw(hx,hy)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
 
