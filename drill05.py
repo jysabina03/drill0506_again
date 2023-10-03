e8163fb20f88a24b33bf493de200d28504059eb5
jysabina03 <jysabina03@tukorea.ac.kr>
2023-10-03 20:37:46 +0900

    refactoring
    기본 refactoring 완료 (함수로 구조화시킴)

---
+++ drill05.py
@@ -0,0 +1,55 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+
+
+def load_resources():
+    global TUK_ground, character
+    TUK_ground = load_image('TUK_GROUND.png')
+    character = load_image('animation_sheet.png')
+
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
+
+
+def reset_world():
+    global running, x, y, frame
+    running = True
+    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    frame = 0
+
+
+def render_world():
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+
+
+def update_world():
+    global frame
+    frame = (frame + 1) % 8
+
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+hide_cursor()
+load_resources()
+reset_world()
+
+while running:
+    render_world()  # 월드 현재 내용 그리기
+    handle_events()  # 사용자 입력 받아들이기
+    update_world()  # 월드 안 객체들의 상호작용 계산 후 결과 업데이트
+
+close_canvas()
